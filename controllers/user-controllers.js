224b59b5e11638b5e8b47d5d5dca4304eccf90c4
Scott <scott_hoffmann1879@outlook.com>
2022-05-17 12:39:02 -0400

    ready to deploy

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -15,7 +15,6 @@ const Str = require('@supercharge/strings');
 const createUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError(
         'Invalid inputs passed. Make sure all inputs have been filled out.',
@@ -47,11 +46,11 @@ const createUser = async (req, res, next) => {
 
   // if role is client
   let codedUser;
-  let coachId = []
+  let coachId = [];
 
   if (role === 'client' && coachCode) {
     try {
-      codedUser = await User.findOne({code: coachCode})
+      codedUser = await User.findOne({ code: coachCode });
     } catch (err) {
       const error = newHttpError('Couldnt find a coach with this code', 500);
       return next(error);
@@ -63,21 +62,9 @@ const createUser = async (req, res, next) => {
       );
       return next(error);
     } else {
-      coachId = codedUser.id
+      coachId = codedUser.id;
     }
-
-
   }
-
-
-
-
-
-
-
-
-
-
 
   let hashedPassword;
   try {
@@ -86,8 +73,6 @@ const createUser = async (req, res, next) => {
     const error = new HttpError('creashed while creating hashed password', 500);
     return next(error);
   }
-
-
 
   const monthArray = [
     'January',
@@ -141,11 +126,9 @@ const createUser = async (req, res, next) => {
       workouts: [],
       diets: [],
       messages: [],
-      checkins: []
-    }
+      checkins: [],
+    },
   });
-
-
 
   try {
     await createdUser.save();
@@ -157,13 +140,14 @@ const createUser = async (req, res, next) => {
   let token;
   try {
     token = jwt.sign(
-      { userId: createdUser.id,
+      {
+        userId: createdUser.id,
         email: createdUser.email,
-        role: createdUser.role },
+        role: createdUser.role,
+      },
       'supersecret_dont-share',
       { expiresIn: '1h' }
     );
-
   } catch (err) {
     const error = new HttpError('Creating user failed 2 ', 500);
     return next(error);
@@ -175,8 +159,8 @@ const createUser = async (req, res, next) => {
       client: createdUser.id,
       messages: [],
       clientNotifications: 0,
-      coachNotifications: 0
-    })
+      coachNotifications: 0,
+    });
 
     try {
       await createdConvo.save();
@@ -192,12 +176,9 @@ const createUser = async (req, res, next) => {
       workouts: [...codedUser.notifications.workouts],
       diets: [...codedUser.notifications.diets],
       messages: [...codedUser.notifications.messages],
-      checkins: [...codedUser.notifications.checkins]
-    }
+      checkins: [...codedUser.notifications.checkins],
+    };
     createdUser.conversations.push(createdConvo.id);
-
-
-
 
     try {
       createdUser.save();
@@ -209,27 +190,28 @@ const createUser = async (req, res, next) => {
     try {
       codedUser.save();
     } catch (err) {
-      const error = new HttpError('could not update coach list of clients', 500);
+      const error = new HttpError(
+        'could not update coach list of clients',
+        500
+      );
       return next(error);
     }
 
     try {
       token = jwt.sign(
-        { userId: createdUser.id,
+        {
+          userId: createdUser.id,
           email: createdUser.email,
-          role: createdUser.role
+          role: createdUser.role,
         },
-        'supersecret_dont-share',
+        'supersecret_dont_share',
         { expiresIn: '1h' }
       );
-
     } catch (err) {
       const error = new HttpError('Creating user failed 2 ', 500);
       return next(error);
     }
-
   }
-
 
   res.status(201).json({
     userId: createdUser.id,
@@ -240,14 +222,9 @@ const createUser = async (req, res, next) => {
   });
 };
 
-
-
-
-
 const login = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError(
         'Invalid inputs passed. Make sure all inputs have been filled out.',
@@ -315,11 +292,11 @@ const login = async (req, res, next) => {
 };
 
 const tempUpdate = async (req, res, next) => {
-  const userId = req.params.uid
+  const userId = req.params.uid;
 
   let user;
   try {
-  user = await User.findById(userId)
+    user = await User.findById(userId);
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not find this user',
@@ -333,26 +310,21 @@ const tempUpdate = async (req, res, next) => {
     workouts: [],
     diets: [],
     messages: [],
-    checkins: []
-  }
+    checkins: [],
+  };
 
   try {
-  await user.save();
-} catch (err) {
-  const error = new HttpError(`Could not update user ${err}`, 500);
-  return next(error);
-}
-res.status(200).json({ user: user.toObject({ getters: true }) });
-}
-
-
-
-
+    await user.save();
+  } catch (err) {
+    const error = new HttpError(`Could not update user ${err}`, 500);
+    return next(error);
+  }
+  res.status(200).json({ user: user.toObject({ getters: true }) });
+};
 
 const updateNotifications = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError(
         'Invalid inputs passed. Make sure all inputs have been filled out.',
@@ -360,87 +332,79 @@ const updateNotifications = async (req, res, next) => {
       )
     );
   }
-const userId = req.params.uid
-const {workout, diet, client, message, checkin} = req.body
+  const userId = req.params.uid;
+  const { workout, diet, client, message, checkin } = req.body;
 
-let user;
-try {
-  user = await User.findById(userId)
-} catch (err) {
-  const error = new HttpError(
-    'Something went wrong, could not find this user',
-    500
-  );
-  return next(error);
-}
-
-
-if (workout) {
-  user.notifications = {
-    clients: [...user.notifications.clients],
-    workouts: user.notifications.workouts.filter(item => item !== workout),
-    diets: [...user.notifications.diets],
-    messages: [...user.notifications.messages],
-    checkins: [...user.notifications.checkins]
+  let user;
+  try {
+    user = await User.findById(userId);
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not find this user',
+      500
+    );
+    return next(error);
   }
 
-}
-if (diet) {
-  user.notifications = {
-    clients: [...user.notifications.clients],
-    workouts: [...user.notifications.workouts],
-    diets: user.notifications.diets.filter(item => item !== diet),
-    messages: [...user.notifications.messages],
-    checkins: [...user.notifications.checkins]
+  if (workout) {
+    user.notifications = {
+      clients: [...user.notifications.clients],
+      workouts: user.notifications.workouts.filter((item) => item !== workout),
+      diets: [...user.notifications.diets],
+      messages: [...user.notifications.messages],
+      checkins: [...user.notifications.checkins],
+    };
   }
-}
-if (client) {
-  user.notifications = {
-    clients: user.notifications.clients.filter(item => item !== client),
-    workouts: [...user.notifications.workouts],
-    diets: [...user.notifications.diets],
-    messages: [...user.notifications.messages],
-    checkins: [...user.notifications.checkins]
+  if (diet) {
+    user.notifications = {
+      clients: [...user.notifications.clients],
+      workouts: [...user.notifications.workouts],
+      diets: user.notifications.diets.filter((item) => item !== diet),
+      messages: [...user.notifications.messages],
+      checkins: [...user.notifications.checkins],
+    };
   }
-}
-
-if (message) {
-  user.notifications = {
-    clients: [...user.notifications.clients],
-    workouts: [...user.notifications.workouts],
-    diets: [...user.notifications.diets],
-    messages: user.notifications.messages.filter(item => item !== message),
-    checkins: [...user.notifications.checkins]
+  if (client) {
+    user.notifications = {
+      clients: user.notifications.clients.filter((item) => item !== client),
+      workouts: [...user.notifications.workouts],
+      diets: [...user.notifications.diets],
+      messages: [...user.notifications.messages],
+      checkins: [...user.notifications.checkins],
+    };
   }
-}
-if (checkin) {
-  user.notifications = {
-    clients: [...user.notifications.clients],
-    workouts: [...user.notifications.workouts],
-    diets: [...user.notifications.diets],
-    messages: [...user.notifications.messages],
-    checkins: user.notifications.checkins.filter(item => item !== checkin)
+
+  if (message) {
+    user.notifications = {
+      clients: [...user.notifications.clients],
+      workouts: [...user.notifications.workouts],
+      diets: [...user.notifications.diets],
+      messages: user.notifications.messages.filter((item) => item !== message),
+      checkins: [...user.notifications.checkins],
+    };
   }
-}
+  if (checkin) {
+    user.notifications = {
+      clients: [...user.notifications.clients],
+      workouts: [...user.notifications.workouts],
+      diets: [...user.notifications.diets],
+      messages: [...user.notifications.messages],
+      checkins: user.notifications.checkins.filter((item) => item !== checkin),
+    };
+  }
 
-try {
-  await user.save();
-} catch (err) {
-  const error = new HttpError(`Could not update user ${err}`, 500);
-  return next(error);
-}
-res.status(200).json({ user: user.toObject({ getters: true }) });
-}
-
-
-
-
-
+  try {
+    await user.save();
+  } catch (err) {
+    const error = new HttpError(`Could not update user ${err}`, 500);
+    return next(error);
+  }
+  res.status(200).json({ user: user.toObject({ getters: true }) });
+};
 
 const updateUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError(
         'Invalid inputs passed. Make sure all inputs have been filled out.',
@@ -470,7 +434,6 @@ const updateUser = async (req, res, next) => {
       uploadedResponse = await cloudinary.uploader.upload(image, {
         upload_preset: 'coach-production',
       });
-
     } catch (err) {
       const error = new HttpError(
         'Couldnt upload this image to cloudinary',
@@ -495,8 +458,6 @@ const updateUser = async (req, res, next) => {
     user.gender = gender;
   }
   user.checkins = [];
-
-
 
   try {
     await user.save();
@@ -559,7 +520,6 @@ const getClients = async (req, res, next) => {
 const addClient = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError('Is this a real person? Is this the matrix?', 422)
     );
@@ -644,7 +604,6 @@ const addClient = async (req, res, next) => {
 const removeClient = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError('Is this a real person? Is this the matrix?', 422)
     );
@@ -740,15 +699,9 @@ const removeClient = async (req, res, next) => {
   });
 };
 
-
-
-
-
-
 const getAllUserData = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-
     return next(
       new HttpError('Is this a real person? Is this the matrix?', 422)
     );
@@ -814,7 +767,6 @@ const getAllUserData = async (req, res, next) => {
     return next(error);
   }
 
-
   try {
     checkins = await Checkin.find({ client: user.clients });
   } catch (err) {
@@ -836,17 +788,97 @@ const getAllUserData = async (req, res, next) => {
     const error = new HttpError('couldnt find the checkins of your clients');
     return next(error);
   }
-  let orderedCheckins = clientCheckins.sort(function (a, b) {
+  // let orderedCheckins = clientCheckins.sort(function (a, b) {
+  //   return a.date.time - b.date.time;
+  // });
+
+  // let clientTotals = [];
+  // let workoutTotals = [];
+  // let dietTotals = [];
+  // let checkinTotals = [];
+  // // this is just the client id. Need whole client info.
+  // if (clients) {
+  //   for (let i = 0; i < clients.length; i++) {
+  //     clientTotals.push({
+  //       value: i + 1,
+  //       date: `${clients[i].dateJoined.monthString.slice(0, 3).toUpperCase()} ${
+  //         clients[i].dateJoined.day
+  //       }`,
+  //     });
+  //   }
+  // }
+
+  // if (workouts && workouts.length > 0) {
+  //   for (let i = 0; i < workouts.length; i++) {
+  //     workoutTotals.push({
+  //       value: i + 1,
+  //       date: `${workouts[i].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+  //         workouts[i].dateAdded.day
+  //       }`,
+  //     });
+  //   }
+  // }
+
+  // if (diets && diets.length > 0) {
+  //   for (let i = 0; i < diets.length; i++) {
+  //     dietTotals.push({
+  //       value: i + 1,
+  //       date: `${diets[i].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+  //         diets[i].dateAdded.day
+  //       }`,
+  //     });
+  //   }
+  // }
+
+  // if (clientCheckins && clientCheckins.length > 0) {
+  //   for (let i = 0; i < orderedCheckins.length; i++) {
+  //     checkinTotals.push({
+  //       value: i + 1,
+  //       date: `${orderedCheckins[i].date.monthString
+  //         .slice(0, 3)
+  //         .toUpperCase()} ${orderedCheckins[i].date.day}`,
+  //     });
+  //   }
+  // }
+
+  let orderedCheckins
+  let orderedDiets
+  let orderedWorkouts
+  let orderedClients
+
+if (checkins) {
+  orderedCheckins = checkins.sort(function (a, b) {
     return a.date.time - b.date.time;
   });
+}
+
+if (workouts) {
+  orderedWorkouts = workouts.sort(function (a, b) {
+    return a.dateAdded.time - b.dateAdded.time;
+  });
+
+}
+  if (diets) {
+  orderedDiets = diets.sort(function (a, b) {
+      return b.dateAdded.time - a.dateAdded.time;
+    });
+  }
+
+if(clients) {
+   orderedClients = clients.sort(function (a, b) {
+    return a.dateJoined.time - b.dateJoined.time;
+  });
+}
+
 
   let clientTotals = [];
   let workoutTotals = [];
   let dietTotals = [];
   let checkinTotals = [];
 // this is just the client id. Need whole client info.
-  if (clients) {
-    for (let i = 0; i < clients.length; i++) {
+if (clients && clients.length > 1) {
+  for (let i = 1; i < orderedClients.length; i++) {
+    if(i === orderedClients.length -1) {
       clientTotals.push({
         value: i + 1,
         date: `${clients[i].dateJoined.monthString.slice(0, 3).toUpperCase()} ${
@@ -854,42 +886,131 @@ const getAllUserData = async (req, res, next) => {
         }`,
       });
     }
-  }
-
-
-
-  if (workouts && workouts.length > 0) {
-    for (let i = 0; i < workouts.length; i++) {
-      workoutTotals.push({
+    if(orderedClients[i - 1].dateJoined.day !==  orderedClients[i].dateJoined.day) {
+      clientTotals.push({
         value: i + 1,
-        date: `${workouts[i].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
-          workouts[i].dateAdded.day
+        date: `${clients[i-1].dateJoined.monthString.slice(0, 3).toUpperCase()} ${
+          clients[i-1].dateJoined.day
         }`,
       });
     }
+
+
+  }
+}
+
+if(clients.length == 1 ) {
+  clientTotals.push({
+    value: 1,
+    date: `${clients[0].dateJoined.monthString.slice(0, 3).toUpperCase()} ${
+      clients[0].dateJoined.day
+    }`,
+  });
+}
+
+
+  if (workouts && workouts.length > 1) {
+    for (let i = 1; i < orderedWorkouts.length; i++) {
+      if(i === orderedWorkouts.length -1) {
+        workoutTotals.push({
+          value: i + 1,
+          date: `${workouts[i].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+            workouts[i].dateAdded.day
+          }`,
+        });
+      }
+      if(orderedWorkouts[i - 1].dateAdded.day !==  orderedWorkouts[i].dateAdded.day) {
+        workoutTotals.push({
+          value: i + 1,
+          date: `${workouts[i-1].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+            workouts[i-1].dateAdded.day
+          }`,
+        });
+      }
+
+
+    }
+  }
+
+  if(workouts.length == 1 ) {
+    workoutTotals.push({
+      value: 1,
+      date: `${workouts[0].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+        workouts[0].dateAdded.day
+      }`,
+    });
   }
 
   if (diets && diets.length > 0) {
-    for (let i = 0; i < diets.length; i++) {
-      dietTotals.push({
-        value: i + 1,
-        date: `${diets[i].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
-          diets[i].dateAdded.day
-        }`,
-      });
+    for (let i = 1; i < orderedDiets.length; i++) {
+      if(i === orderedDiets.length -1) {
+        dietTotals.push({
+          value: i + 1,
+          date: `${diets[i].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+            diets[i].dateAdded.day
+          }`,
+        });
+      }
+      if(orderedDiets[i - 1].dateAdded.day !==  orderedDiets[i].dateAdded.day) {
+        dietTotals.push({
+          value: i + 1,
+          date: `${diets[i-1].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+            diets[i-1].dateAdded.day
+          }`,
+        });
+      }
+
+
     }
   }
 
-  if (clientCheckins && clientCheckins.length > 0) {
-    for (let i = 0; i < orderedCheckins.length; i++) {
+  if(diets.length == 1 ) {
+    dietTotals.push({
+      value: 1,
+      date: `${diets[0].dateAdded.monthString.slice(0, 3).toUpperCase()} ${
+        diets[0].dateAdded.day
+      }`,
+    });
+  }
+
+
+
+
+
+  if (orderedCheckins && orderedCheckins.length > 0) {
+    for (let i = 1; i < orderedCheckins.length; i++) {
+      if (i === orderedCheckins.length - 1) {
       checkinTotals.push({
         value: i + 1,
         date: `${orderedCheckins[i].date.monthString
           .slice(0, 3)
           .toUpperCase()} ${orderedCheckins[i].date.day}`,
-      });
+      })
+    }
+      if(orderedCheckins[i - 1].date.day !==  orderedCheckins[i].date.day) {
+        checkinTotals.push({
+          value: i + 1,
+          date: `${orderedCheckins[i - 1].date.monthString
+            .slice(0, 3)
+            .toUpperCase()} ${orderedCheckins[i - 1].date.day}`,
+        });
+
+      }
+
     }
   }
+
+  if(checkins.length == 1 ) {
+    checkinTotals.push({
+      value: 1,
+      date: `${checkins[0].date.monthString.slice(0, 3).toUpperCase()} ${
+        checkins[0].date.day
+      }`,
+    });
+  }
+
+  //filter this array to only have one value (the largest) for each date ^
+
 
   let finalCoach = [];
   let finalClients = [];
@@ -898,7 +1019,6 @@ const getAllUserData = async (req, res, next) => {
   let finalWorkouts = [];
   let finalCheckins = [];
   let finalUserCheckins = [];
-
 
   if (coach) {
     finalCoach = coach.toObject({ getters: true });
@@ -949,7 +1069,7 @@ const getAllUserData = async (req, res, next) => {
     gender: user.gender,
     checkins: finalCheckins,
     userCheckins: finalUserCheckins,
-    notifications: user.notifications
+    notifications: user.notifications,
   });
 };
 
@@ -961,6 +1081,6 @@ exports.getAllUsers = getAllUsers;
 exports.updateUser = updateUser;
 exports.login = login;
 exports.createUser = createUser;
-exports.updateNotifications = updateNotifications
+exports.updateNotifications = updateNotifications;
 
 exports.tempUpdate = tempUpdate;
